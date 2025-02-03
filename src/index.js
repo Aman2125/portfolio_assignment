d910e364b67540c4b2ce4ed4026988595d3ff18b
Aman2125 <amankumar943087@gmail.com>
2025-02-03 20:55:58 +0530

    updated index for compatibility

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 /**
- * Entry point of application, where App is rendered within the div with the id of "app" 
+ * Entry point of application, where App is rendered within the div with the id of "app"
  */
 
 import React from "react";
-import { render } from "react-dom";
-
+import { createRoot } from "react-dom/client"; // Import createRoot instead of render
 import App from "./App";
 
-render(<App></App>, document.getElementById("app"));
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
